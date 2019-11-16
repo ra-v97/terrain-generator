2c183cff8d673d68dd764a23da01bebdc7653786
luknw <luknwpl@gmail.com>
2019-11-16 21:13:45 +0100

    Mark triangles for refinement

--- src/main/java/app/MainApp.java
+++ src/main/java/app/MainApp.java
@@ -5,9 +5,6 @@ import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.javatuples.Pair;
 import processor.MapProcessingUtil;
-import transformation.Transformation;
-import transformation.TransformationP1;
-import transformation.TransformationP7;
 
 public class MainApp {
 
@@ -61,7 +58,7 @@ public class MainApp {
         return new Pair<>(graph, in1);
     }
 
-    private static Pair<ModelGraph, InteriorNode> task15(){
+    private static Pair<ModelGraph, InteriorNode> task15() {
         ModelGraph graph = new ModelGraph("testGraph");
         Vertex v1 = graph.insertVertex("v1", VertexType.SIMPLE_NODE, new Point3d(0.0, 0.0, 0.0));
         Vertex h2 = graph.insertVertex("h2", VertexType.HANGING_NODE, new Point3d(1.0, 0.0, 0.0));
@@ -120,35 +117,36 @@ public class MainApp {
     public static void main(String[] args) {
         BasicConfigurator.configure();
 
-        Pair<ModelGraph, InteriorNode> task = task1();
-        ModelGraph graph = task.getValue0();
-        InteriorNode interiorNode = task.getValue1();
-
-        Transformation t1 = new TransformationP1();
-        log.info(String.format("Condition state for transformation P1: %b", t1.isConditionCompleted(graph, interiorNode)));
-
-        graph.display();
-
-        try {
-            Thread.sleep(3000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        t1.transformGraph(graph, interiorNode);
-
-//        TerrainMap map = new TerrainMap();
-//        map.fillMapWithExampleData();
+//        Pair<ModelGraph, InteriorNode> task = task1();
+//        ModelGraph graph = task.getValue0();
+//        InteriorNode interiorNode = task.getValue1();
 //
-//        ModelGraph graph = new ModelGraph("testGraph");
-//        Vertex v1 = graph.insertVertex("v1", VertexType.SIMPLE_NODE, new Point3d(0.0, 0.0, 2.0));
-//        Vertex v2 = graph.insertVertex("v2", VertexType.SIMPLE_NODE, new Point3d(5.0, 0.0, 2.0));
-//        Vertex v3 = graph.insertVertex("v3", VertexType.HANGING_NODE, new Point3d(0.0, 3.0, 2.0));
-//        graph.insertEdge("e1", v1, v2, true);
-//        graph.insertEdge("e2", v2, v3, true);
-//        graph.insertEdge("e3", v3, v1, true);
-//        InteriorNode in1 = graph.insertInterior("i1", v1, v2, v3);
+//        Transformation t1 = new TransformationP1();
+//        log.info(String.format("Condition state for transformation P1: %b", t1.isConditionCompleted(graph, interiorNode)));
 //
-//        System.out.println(map.getAllPointsInTriangleArea(in1).size());
-//        System.out.println(MapProcessingUtil.calculateTerrainApproximationError(in1, map));
+//        graph.display();
+//
+//        try {
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//        t1.transformGraph(graph, interiorNode);
+
+        TerrainMap map = new TerrainMap(5, 5);
+        map.fillMapWithExampleData();
+
+        ModelGraph graph = new ModelGraph("testGraph");
+        Vertex v1 = graph.insertVertex("v1", VertexType.SIMPLE_NODE, new Point3d(0.0, 0.0, 2.0));
+        Vertex v2 = graph.insertVertex("v2", VertexType.SIMPLE_NODE, new Point3d(5.0, 0.0, 2.0));
+        Vertex v3 = graph.insertVertex("v3", VertexType.HANGING_NODE, new Point3d(0.0, 3.0, 2.0));
+        graph.insertEdge("e1", v1, v2, true);
+        graph.insertEdge("e2", v2, v3, true);
+        graph.insertEdge("e3", v3, v1, true);
+        InteriorNode in1 = graph.insertInterior("i1", v1, v2, v3);
+
+        System.out.println(map.getAllPointsInTriangleArea(in1).size());
+        System.out.println(MapProcessingUtil.calculateTerrainApproximationError(in1, map));
+        System.out.println(MapProcessingUtil.markTrianglesForRefinement(map, 1 - 1e-7));
     }
 }
--- src/main/java/model/TerrainMap.java
+++ src/main/java/model/TerrainMap.java
@@ -9,16 +9,20 @@ public class TerrainMap {
 
     private static final double EPSILON = Double.MIN_VALUE;
 
-    private static final int MAP_HIGH = 5;
+    private final int sizeX;
+    private final int sizeY;
+    private final Point3d[][] points;
 
-    private static final int MAP_WIDHT = 5;
-
-    private final Point3d[][] terrainMap = new Point3d[MAP_HIGH][MAP_WIDHT];
+    public TerrainMap(int sizeX, int sizeY) {
+        this.sizeX = sizeX;
+        this.sizeY = sizeY;
+        points = new Point3d[this.sizeX][this.sizeY];
+    }
 
     public void fillMapWithExampleData() {
-        for (int i = 0; i < MAP_HIGH; i++) {
-            for (int j = 0; j < MAP_WIDHT; j++) {
-                terrainMap[i][j] = new Point3d(i, j, 1.0);
+        for (int i = 0; i < this.sizeX; i++) {
+            for (int j = 0; j < this.sizeY; j++) {
+                points[i][j] = new Point3d(i, j, 1.0);
             }
         }
     }
@@ -30,16 +34,16 @@ public class TerrainMap {
 
         TriangleSquareBorder triangleSquareBorder = new TriangleSquareBorder(triangle);
 
-        for (startIndexI = 1; terrainMap[startIndexI][0].getY() < triangleSquareBorder.getMinY() && startIndexI <= MAP_HIGH; startIndexI++){}
+        for (startIndexI = 1; points[startIndexI][0].getY() < triangleSquareBorder.getMinY() && startIndexI <= sizeX; startIndexI++){}
         startIndexI--;
 
-        for (startIndexJ = 1; terrainMap[0][startIndexJ].getX() < triangleSquareBorder.getMinX() && startIndexJ <= MAP_WIDHT; startIndexJ++){}
+        for (startIndexJ = 1; points[0][startIndexJ].getX() < triangleSquareBorder.getMinX() && startIndexJ <= sizeY; startIndexJ++){}
         startIndexJ--;
 
-        for (int i = startIndexI; i < MAP_HIGH && terrainMap[i][0].getY() < triangleSquareBorder.getMaxY(); i++) {
-            for (int j = startIndexJ; j < MAP_WIDHT && terrainMap[0][j].getX() < triangleSquareBorder.getMaxX(); j++) {
-                if (checkIfPointIsInsideTriangle(triangle, terrainMap[i][j])) {
-                    trianglePoints.add(terrainMap[i][j]);
+        for (int i = startIndexI; i < this.sizeX && points[i][0].getY() < triangleSquareBorder.getMaxY(); i++) {
+            for (int j = startIndexJ; j < this.sizeY && points[0][j].getX() < triangleSquareBorder.getMaxX(); j++) {
+                if (checkIfPointIsInsideTriangle(triangle, points[i][j])) {
+                    trianglePoints.add(points[i][j]);
                 }
             }
         }
@@ -61,6 +65,18 @@ public class TerrainMap {
 
     private static double calculate2DTriangleArenaForXY(Point3d v1, Point3d v2, Point3d v3) {
         return Math.abs(v1.getX() * (v2.getY() - v3.getY()) + v2.getX() * (v3.getY() - v1.getY()) + v3.getX() * (v1.getY() - v2.getY()) / 2.0);
+    }
+
+    public int getSizeX() {
+        return sizeX;
+    }
+
+    public int getSizeY() {
+        return sizeY;
+    }
+
+    public Point3d[][] getPoints() {
+        return points;
     }
 
     public static final class TriangleSquareBorder {
