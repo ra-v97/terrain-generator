5d169197b7813752a199559e0194c1524ee17469
ostro <ostr00000@gmail.com>
2019-11-27 01:00:16 +0100

    change implementation of P6 transformation

--- src/main/java/transformation/TransformationP6.java
+++ src/main/java/transformation/TransformationP6.java
@@ -1,151 +1,113 @@
 package transformation;
 
 import model.*;
+import org.javatuples.Pair;
 import org.javatuples.Triplet;
 
-import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 
 public class TransformationP6 implements Transformation {
 
-    private static final String SIMPLE_VERTEX_1 = "vertex1";
-    private static final String SIMPLE_VERTEX_3 = "vertex3";
-    private static final String SIMPLE_VERTEX_5 = "vertex5";
-    private static final String HANGING_NODE_2 = "node2";
-    private static final String HANGING_NODE_4 = "node4";
-
-    private static final String SIMPLE_VERTEX_2 = "vertex2";
-
-
-    private Map<String, Vertex> mapVerticesToModel(ModelGraph graph, InteriorNode interiorNode){
-        Map<String, Vertex> verticesMap = new HashMap<>();
-
-        Triplet<Vertex, Vertex, Vertex> triangleSimple = interiorNode.getTriangleVertexes();
-        Vertex vertexA = triangleSimple.getValue0();
-        Vertex vertexB = triangleSimple.getValue1();
-        Vertex vertexC = triangleSimple.getValue2();
-
-        Vertex vertexD = graph.getVertexBetween(vertexA, vertexB).orElse(null);
-        Vertex vertexE = graph.getVertexBetween(vertexB, vertexC).orElse(null);
-        Vertex vertexF = graph.getVertexBetween(vertexA, vertexC).orElse(null);
-
-        if(vertexD == null){
-            verticesMap.put(SIMPLE_VERTEX_1, vertexA);
-            verticesMap.put(SIMPLE_VERTEX_5, vertexB);
-            verticesMap.put(SIMPLE_VERTEX_3, vertexC);
-        }
-
-        if(vertexE == null){
-            verticesMap.put(SIMPLE_VERTEX_1, vertexB);
-            verticesMap.put(SIMPLE_VERTEX_5, vertexC);
-            verticesMap.put(SIMPLE_VERTEX_3, vertexA);
-        }
-
-        if(vertexF == null){
-            verticesMap.put(SIMPLE_VERTEX_1, vertexA);
-            verticesMap.put(SIMPLE_VERTEX_5, vertexC);
-            verticesMap.put(SIMPLE_VERTEX_3, vertexB);
-        }
-
-        List<Vertex> hangingNodes = interiorNode.getAssociatedNodes();
-
-        Vertex hanging2;
-        Vertex hanging4;
-
-        if(verticesMap.get(SIMPLE_VERTEX_1).getEdgeBetween(hangingNodes.get(0)) != null){
-            hanging2 = hangingNodes.get(0);
-            hanging4 = hangingNodes.get(1);
-        }
-        else{
-            hanging2 = hangingNodes.get(1);
-            hanging4 = hangingNodes.get(0);
-        }
-
-        verticesMap.put(HANGING_NODE_2, hanging2);
-        verticesMap.put(HANGING_NODE_4, hanging4);
-
-        return verticesMap;
+    private static class InvalidProduction extends IllegalStateException {
     }
 
     @Override
-    public boolean isConditionCompleted(ModelGraph graph, InteriorNode interiorNode){
-        List<Vertex> hangingNodes = interiorNode.getAssociatedNodes();
-
-        if(hangingNodes.size() != 2){
-            return false;
-        }
-        Map<String, Vertex> verticesMap = this.mapVerticesToModel(graph, interiorNode);
-
-        if(verticesMap.size() != 5){
-            System.out.println(verticesMap.size());
+    public boolean isConditionCompleted(ModelGraph graph, InteriorNode interiorNode) {
+        try {
+            convertToProductionModel(graph, interiorNode);
+        } catch (InvalidProduction invalidProduction) {
             return false;
         }
 
-        Triplet<Vertex, Vertex, Vertex> triangle = interiorNode.getTriangleVertexes();
-        int hanging_nodes = interiorNode.getAssociatedNodes().size();
+        return true;
+    }
 
-        if (getSimpleVertexCount(triangle) != 3 || hanging_nodes != 2) {
-            return false;
+    private Pair<List<GraphEdge>, List<Vertex>>
+    convertToProductionModel(ModelGraph graph, InteriorNode interiorNode) throws InvalidProduction {
+        Triplet<Vertex, Vertex, Vertex> triangle = interiorNode.getTriangle();
+        Vertex v0 = triangle.getValue0();
+        Vertex v2 = triangle.getValue1();
+        Vertex v4 = triangle.getValue2();
+        Vertex v1 = this.getMiddleVertex(graph, v0, v2);
+        Vertex v3 = this.getMiddleVertex(graph, v2, v4);
+        Vertex v5 = this.getMiddleVertex(graph, v4, v0);
+
+        LinkedList<Vertex> vertices = new LinkedList<>();
+        vertices.add(v0);
+        vertices.add(v1);
+        vertices.add(v2);
+        vertices.add(v3);
+        vertices.add(v4);
+        vertices.add(v5);
+
+        GraphEdge e0 = getEdgeBetween(graph, v0, v1);
+        GraphEdge e1 = getEdgeBetween(graph, v1, v2);
+        GraphEdge e2 = getEdgeBetween(graph, v2, v3);
+        GraphEdge e3 = getEdgeBetween(graph, v3, v4);
+        GraphEdge e4 = getEdgeBetween(graph, v4, v5);
+        GraphEdge e5 = getEdgeBetween(graph, v5, v0);
+
+        LinkedList<GraphEdge> edges = new LinkedList<>();
+        edges.add(e0);
+        edges.add(e1);
+        edges.add(e2);
+        edges.add(e3);
+        edges.add(e4);
+        edges.add(e5);
+
+        double base0 = e0.getL() + e1.getL();
+        double base1 = e2.getL() + e3.getL();
+        double base2 = e4.getL() + e5.getL();
+
+        if (base1 > base0 && base1 > base2) {
+            vertices.addLast(vertices.removeFirst());
+            vertices.addLast(vertices.removeFirst());
+            edges.addLast(edges.removeFirst());
+            edges.addLast(edges.removeFirst());
+
+        } else if (base2 > base0 && base2 > base1) {
+            vertices.addFirst(vertices.removeLast());
+            vertices.addFirst(vertices.removeLast());
+            edges.addFirst(edges.removeLast());
+            edges.addFirst(edges.removeLast());
         }
 
-        GraphEdge e1 = graph.getEdgeBetweenNodes(verticesMap.get(SIMPLE_VERTEX_1), verticesMap.get(HANGING_NODE_2))
-                .orElseThrow(() -> new IllegalStateException("Unknown vertices"));
+        return Pair.with(edges, vertices);
+    }
 
-        GraphEdge e2 = graph.getEdgeBetweenNodes(verticesMap.get(HANGING_NODE_2), verticesMap.get(SIMPLE_VERTEX_3))
-                .orElseThrow(() -> new IllegalStateException("Unknown vertices"));
+    private static GraphEdge getEdgeBetween(ModelGraph modelGraph, Vertex begin, Vertex end) {
+        return modelGraph.getEdgeBetweenNodes(begin, end).orElseThrow(InvalidProduction::new);
+    }
 
-        GraphEdge e3 = graph.getEdgeBetweenNodes(verticesMap.get(SIMPLE_VERTEX_3), verticesMap.get(HANGING_NODE_4))
-                .orElseThrow(() -> new IllegalStateException("Unknown vertices"));
-
-        GraphEdge e4 = graph.getEdgeBetweenNodes(verticesMap.get(HANGING_NODE_4), verticesMap.get(SIMPLE_VERTEX_5))
-                .orElseThrow(() -> new IllegalStateException("Unknown vertices"));
-
-        GraphEdge e5 = graph.getEdgeBetweenNodes(verticesMap.get(SIMPLE_VERTEX_1), verticesMap.get(SIMPLE_VERTEX_5))
-                .orElseThrow(() -> new IllegalStateException("Unknown vertices"));
-
-        return (e1.getL() + e2.getL() >= (e3.getL() + e4.getL())) && (e1.getL() + e2.getL() >= e5.getL());
+    private Vertex getMiddleVertex(ModelGraph graph, Vertex begin, Vertex end) throws InvalidProduction {
+        return graph.getVertexesBetween(begin, end)
+                .stream()
+                .filter(v -> v.getVertexType() == VertexType.HANGING_NODE)
+                .findAny()
+                .orElseThrow(InvalidProduction::new);
     }
 
     @Override
-    public ModelGraph transformGraph(ModelGraph graph, InteriorNode interiorNode) {
+    public ModelGraph transformGraph(ModelGraph graph, InteriorNode interiorNode) throws InvalidProduction {
+        Vertex[] v = this.convertToProductionModel(graph, interiorNode).getValue1().toArray(new Vertex[0]);
 
-        Map<String, Vertex> verticesMap = this.mapVerticesToModel(graph, interiorNode);
+        graph.removeInterior(interiorNode.getId());
+        graph.insertInterior(this.generateId(v[0], v[1], v[4]), v[0], v[1], v[4]);
+        graph.insertInterior(this.generateId(v[1], v[2], v[4]), v[1], v[2], v[4]);
 
-        if(this.isConditionCompleted(graph, interiorNode)){
-
-            graph.removeInterior(interiorNode.getId());
-
-            //change type of node 2
-            Vertex node2 = verticesMap.get(HANGING_NODE_2);
-            node2.setVertexType(VertexType.SIMPLE_NODE);
-            verticesMap.remove(HANGING_NODE_2);
-            verticesMap.put(SIMPLE_VERTEX_2, node2);
-
-            graph.insertEdge("edge6", verticesMap.get(SIMPLE_VERTEX_2), verticesMap.get(SIMPLE_VERTEX_5), false);
-
-            //insert new interiors
-            String leftInteriorId = verticesMap.get(SIMPLE_VERTEX_1).getId() + verticesMap.get(SIMPLE_VERTEX_2).getId() + verticesMap.get(SIMPLE_VERTEX_5).getId();
-            String rightInteriorId = verticesMap.get(SIMPLE_VERTEX_2).getId() + verticesMap.get(SIMPLE_VERTEX_3).getId() + verticesMap.get(SIMPLE_VERTEX_5).getId();
-
-            InteriorNode leftInteriorNode = graph.insertInterior(leftInteriorId, verticesMap.get(SIMPLE_VERTEX_1), verticesMap.get(SIMPLE_VERTEX_2), verticesMap.get(SIMPLE_VERTEX_5));
-            leftInteriorNode.setPartitionRequired(false);
-
-            InteriorNode rightInteriorNode = graph.insertInterior(rightInteriorId, verticesMap.get(SIMPLE_VERTEX_2), verticesMap.get(SIMPLE_VERTEX_3), verticesMap.get(SIMPLE_VERTEX_5), verticesMap.get(HANGING_NODE_4));
-            rightInteriorNode.setPartitionRequired(false);
-        }
+        GraphEdge edge = graph.insertEdge(this.generateId(v[1], v[4]), v[1], v[4]);
+        edge.setB(false);
+        v[1].setVertexType(VertexType.SIMPLE_NODE);
 
         return graph;
     }
 
-    private static int getSimpleVertexCount(Triplet<Vertex, Vertex, Vertex> triangle) {
-        int count = 0;
-        for (Object o : triangle) {
-            Vertex v = (Vertex) o;
-            if (v.getVertexType() == VertexType.SIMPLE_NODE) {
-                count++;
-            }
-        }
-        return count;
+    private String generateId(Vertex v1, Vertex v2, Vertex v3) {
+        return v1.getId().concat(v2.getId()).concat(v3.getId());
+    }
+
+    private String generateId(Vertex v1, Vertex v2) {
+        return v1.getId().concat(v2.getId());
     }
 }
